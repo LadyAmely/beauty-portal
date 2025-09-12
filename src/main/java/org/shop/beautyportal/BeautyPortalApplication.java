8aac8dcc45fa7b6bce62be53c28773b1786e4eaf
Amelia <nawrotamelia3@gmail.com>
2025-09-12 20:25:17 +0200

    feat: create new branch

---
+++ src/main/java/org/shop/beautyportal/BeautyPortalApplication.java
@@ -0,0 +1,13 @@
+package org.shop.beautyportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shop/beautyportal/BeautyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shop.beautyportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
