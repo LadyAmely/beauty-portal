727724a95dd29318f30be61003ecde882bf12cde
Amelia <nawrotamelia3@gmail.com>
2025-09-22 02:14:27 +0200

    feat: add craco

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,21 +1,33 @@
 
-
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import path from "path";
+
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-docs"
-  ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
-  },
-  "staticDirs": [
-    "..\\public"
-  ]
+    stories: [
+        "../src/**/*.mdx",
+        "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-docs"
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {}
+    },
+    staticDirs: [
+        "../public"
+    ],
+    webpackFinal: async (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@atoms": path.resolve(__dirname, "../src/components/atoms"),
+            "@molecules": path.resolve(__dirname, "../src/components/molecules"),
+            "@organisms": path.resolve(__dirname, "../src/components/organisms"),
+            "@utils": path.resolve(__dirname, "../src/utils"),
+        };
+        return config;
+    }
 };
+
 export default config;
