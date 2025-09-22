014148ffc3365e47ffea4ce82027ebfff038c54c
Amelia <nawrotamelia3@gmail.com>
2025-09-22 10:00:49 +0200

    aws cogntio configuration

---
+++ src/main/java/org/shop/beautyportal/config/CognitoLogoutHandler.java
@@ -0,0 +1,39 @@
+package org.shop.beautyportal.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Cognito has a custom logout url.
+ * See more information <a href="https://docs.aws.amazon.com/cognito/latest/developerguide/logout-endpoint.html">here</a>.
+ */
+public class CognitoLogoutHandler extends SimpleUrlLogoutSuccessHandler {
+
+
+    @Value("${cognito.domain}")
+    private String domain;
+
+    @Value("${spring.security.oauth2.client.registration.cognito.redirect-uri}")
+    private String logoutRedirectUrl;
+
+    @Value("${spring.security.oauth2.client.registration.cognito.client-id}")
+    private String userPoolClientId;
+
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        return UriComponentsBuilder
+                .fromUri(URI.create(domain + "/logout"))
+                .queryParam("client_id", userPoolClientId)
+                .queryParam("logout_uri", logoutRedirectUrl)
+                .encode(StandardCharsets.UTF_8)
+                .build()
+                .toUriString();
+    }
+}
