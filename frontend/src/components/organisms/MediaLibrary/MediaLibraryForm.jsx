f176f4c0e261338f78a21b780efaa899ecbc6791
Amelia <nawrotamelia3@gmail.com>
2025-09-24 01:08:16 +0200

    feat: add select

--- frontend/src/components/organisms/MediaLibrary/MediaLibraryForm.jsx
+++ frontend/src/components/organisms/MediaLibrary/MediaLibraryForm.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import MediaLibraryFormPropTypes from "./MediaLibraryForm.propTypes";
-import InputGroup from "../../molecules/InputGroup/InputGroup";
 import SelectGroup from "../../molecules/SelectGroup/SelectGroup";
 import TopBar from "../../atoms/TopBar/TopBar";
 import Footer from "../../atoms/Footer/Footer";
 import "./MediaLibraryForm.scss";
 import MediaFileGrid from "../../molecules/Media/MediaFileGrid/MediaFileGrid";
+import TableToolbar from "../../molecules/TableToolbar/TableToolbar";
+import SearchBar from "../../atoms/SearchBar/SearchBar";
 
 const MediaLibraryForm = ({
                               title,
-                              searchValue,
-                              onSearchChange,
                               sortValue,
                               onSortChange,
                               sortOptions,
@@ -20,22 +19,12 @@ const MediaLibraryForm = ({
     return (
         <div className="media-library-form">
             <h2>{title}</h2>
-            <TopBar className="media-library-form__topbar">
-                <InputGroup
-                    label="Search by SKU"
-                    name="skuSearch"
-                    value={searchValue}
-                    onChange={onSearchChange}
-                    placeholder="e.g. SKU123"
-                />
-                <SelectGroup
-                    label="Sort by"
-                    name="sort"
-                    options={sortOptions}
-                    value={sortValue}
-                    onChange={onSortChange}
-                />
-            </TopBar>
+            <TableToolbar title="Media Library" />
+            <section className="purchase-report-form__table-section">
+                <TopBar className="media-library-form__topbar">
+                    <SearchBar value="" />
+                </TopBar>
+            </section>
             <MediaFileGrid fileList={fileList} />
             {footerNote && (
                 <Footer className="media-library-form__footer">{footerNote}</Footer>
